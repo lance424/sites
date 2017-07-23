0883ccafca2f9edb4e0d04a251d958a7ac7119d1
lance424 <30356010+lance424@users.noreply.github.com>
2017-07-22 22:12:12 -0500

    Update index.php

--- ajmlp/dev/index.php
+++ ajmlp/dev/index.php
@@ -18,6 +18,7 @@
         <h3>Heading 3</h3>
         <h4>Heading 4</h4>
         <h5>Heading 5</h5>
+        <h6>Heading 6</h6>
         <h3>Paragraph Heading 3</h3>
         <p>Contact for the website.</p>
         <h3>Second Paragraph Heading 3</h3>
