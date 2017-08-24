0f7155afe8a3f485593f05dcd4f7cb84a1213978
lance424 <30356010+lance424@users.noreply.github.com>
2017-08-23 22:33:28 -0500

    Update nav.php

--- ajmlp/dev/includes/nav.php
+++ ajmlp/dev/includes/nav.php
@@ -1,5 +1,7 @@
       <div id="nav">
-        <a href="#">Home</a>
+        <a href="http://www.ajmlp.com/dev/index.php">Home</a>
+        <a href="http://www.ajmlp.com/dev/corvette.php">Corvette</a>
+        <a href="http://www.ajmlp.com/dev/woodworking.php">Woodworking</a>
         <a href="#">About</a>
         <a href="#">Contact</a>
       </div> <!-- end nav -->
