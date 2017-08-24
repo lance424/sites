a25733b71987b63e2de0735d3b308bafb86e516d
lance424 <30356010+lance424@users.noreply.github.com>
2017-08-23 22:28:31 -0500

    Update index.php

--- ajmlp/dev/index.php
+++ ajmlp/dev/index.php
@@ -19,11 +19,11 @@
         <h3>Version Control</h3>
         <h2>CMS</h2>
         <h3>Content Management System</h3>
-        <h2><a href="http://www.ajmlp.com/corvette.php">Corvette</a></h2>
+        <h2><a href="http://www.ajmlp.com/dev/corvette.php">Corvette</a></h2>
         <h3>Everything Corvette</h3>
         <h3>C5 2002 Convertible</h3>
         <h3>Restoration</h3>
-        <h2><a href="http://www.ajmlp.com/woodworking.php">Woodworking</a></h2>
+        <h2><a href="http://www.ajmlp.com/dev/woodworking.php">Woodworking</a></h2>
         <h3>Tools</h3>
         <h3>Projects</h3>
       </div> <!-- end content -->
