7c8b602d65531e8cd25afaa1a1138fdba46184f0
lance424 <30356010+lance424@users.noreply.github.com>
2017-08-23 22:20:59 -0500

    Create woodworking.php

---
+++ ajmlp/dev/woodworking.php
@@ -0,0 +1,24 @@
+
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+    <META content="AJMLP.com - Woodworking" name="description">
+    <META name="keywords" content="">
+    <META name="author" content="">
+    <link rel="stylesheet" type="text/css" href="ajmlp.css" media="screen">
+    <title>AJMLP</title>
+  </head>
+  <body>
+    <div id="wrapper">
+      <?php include('includes/header.php'); ?>
+      <?php include('includes/nav.php'); ?>
+      <div id="content">
+        <h1>Woodworking</h1>
+        <h2>Tools</h2>
+        <h2>Projects</h2>
+      </div> <!-- end content -->
+      <?php include('includes/sidebar.php'); ?>
+      <?php include('includes/footer.php'); ?>
+    </div> <!-- end wrapper -->
+  </body>
+</html>
