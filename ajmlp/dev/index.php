52031e57cf45b2761d4c1868d1cf991df72faeed
lance424 <30356010+lance424@users.noreply.github.com>
2017-08-23 22:18:36 -0500

    Update index.php

--- ajmlp/dev/index.php
+++ ajmlp/dev/index.php
@@ -15,15 +15,15 @@
       <div id="content">
         <h1>AJMLP</h1>
         <h3>Consulting</h3>
-        <h2>GITHUB</h2>
+        <h2><a href="https://github.com>GITHUB</a></h2>
         <h3>Version Control</h3>
         <h2>CMS</h2>
         <h3>Content Management System</h3>
-        <h2>Corvette</h2>
+        <h2><a href="http://www.ajmlp.com/corvette.php>Corvette</a></h2>
         <h3>Everything Corvette</h3>
         <h3>C5 2002 Convertible</h3>
         <h3>Restoration</h3>
-        <h2>Woodworking</h2>
+        <h2><a href="http://www.ajmlp.com/woodworking.php>Woodworking</a></h2>
         <h3>Tools</h3>
         <h3>Projects</h3>
       </div> <!-- end content -->
