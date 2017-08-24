05cfa07fb14d9157996776ca301bca7d79879f1c
lance424 <30356010+lance424@users.noreply.github.com>
2017-08-23 22:20:01 -0500

    Create corvette.php

---
+++ ajmlp/dev/corvette.php
@@ -0,0 +1,25 @@
+
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+    <META content="AJMLP.com - Corvette" name="description">
+    <META name="keywords" content="Corvette">
+    <META name="author" content="">
+    <link rel="stylesheet" type="text/css" href="ajmlp.css" media="screen">
+    <title>AJMLP - Corvette</title>
+  </head>
+  <body>
+    <div id="wrapper">
+      <?php include('includes/header.php'); ?>
+      <?php include('includes/nav.php'); ?>
+      <div id="content">
+        <h1>Corvette</h1>
+        <h2>Everything Corvette</h2>
+        <h2>C5 2002 Convertible</h2>
+        <h2>Restoration</h2>
+      </div> <!-- end content -->
+      <?php include('includes/sidebar.php'); ?>
+      <?php include('includes/footer.php'); ?>
+    </div> <!-- end wrapper -->
+  </body>
+</html>
