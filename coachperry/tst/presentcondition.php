d3b66114fda5e3ff708ba6773957b3d4d63e7aca
lance424 <30356010+lance424@users.noreply.github.com>
2018-01-28 17:00:52 -0600

    Add files via upload

---
+++ coachperry/tst/presentcondition.php
@@ -0,0 +1,13 @@
+<?php
+define('IN_PHPBB', true);
+$php_root_dir = './';
+$page = 'presentcondition';
+$pageid = 'presentcondition';
+ini_set("include_path","$php_root_dir:$php_root_dir . 'include':$php_root_dir . 'templates'");
+include($php_root_dir . 'include/config.inc');
+include($php_root_dir . 'templates/head.tpl');
+include($php_root_dir . 'templates/header.tpl');
+include($php_root_dir . 'templates/nav.tpl');
+include($php_root_dir . 'include/content.inc');
+include($php_root_dir . 'templates/footer.tpl');
+?>
